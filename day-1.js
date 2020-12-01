40992f439e98eacebbd3eda1f505081735f0c199
casiimir <73138891+casiimir@users.noreply.github.com>
2020-12-01 20:48:33 +0100

    Update day-1.js

--- day-1.js
+++ day-1.js
@@ -1,4 +1,47 @@
 // Advent of code n. 1
+
+// Specifically, they need you to find the two entries that sum to 2020 and then multiply those
+// two numbers together.
+// For example, suppose your expense report contained the following:
+// 1721
+// 979
+// 366
+// 299
+// 675
+// 1456
+// In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together
+// produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+function challengeOne(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let y = i+1; y <= arr.length; y++) {
+      if ((arr[i] + arr[y]) === 2020) {
+        result = (arr[i] * arr[y]);
+      }
+    }
+  }
+  return result;
+}
+
+function challengeOnePartTwo(arr) {
+  let thirdArr = [];
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let y = i+1; y <= arr.length; y++) {
+      if ((arr[i] + arr[y]) < 2020) {
+        thirdArr.push(arr[i] + arr[y]);
+      }
+      for (let z = i+1; z <= arr.length; z++) {
+        if ((arr[i] + arr[y] + thirdArr[z]) === 2020) {
+          result = ((arr[i] * arr[y] * thirdArr[z]));
+        }
+      }
+    }
+  }
+  return(result);
+}
+
 const arr = (`
 1801
 1324
@@ -201,35 +244,5 @@ const arr = (`
 1965
 1662`).split('\n')
         .map(x => x -0);
-
-function challengeOne(arr) {
-  let result = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let y = i+1; y <= arr.length; y++) {
-      if ((arr[i] + arr[y]) === 2020) {
-        result = (arr[i] * arr[y]);
-      }
-    }
-  }
-  return result;
-}
-
-function challengeOnePartTwo(arr) {
-  let thirdArr = [];
-  let result = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let y = i+1; y <= arr.length; y++) {
-      if ((arr[i] + arr[y]) < 2020) {
-        thirdArr.push(arr[i] + arr[y]);
-      }
-      for (let z = i+1; z <= arr.length; z++) {
-        if ((arr[i] + arr[y] + thirdArr[z]) === 2020) {
-          result = ((arr[i] * arr[y] * thirdArr[z]));
-        }
-      }
-    }
-  }
-  return(result);
-}
 
 console.log (`Step one: ${challengeOne(arr)} - Step two: ${challengeOnePartTwo(arr)}`);
